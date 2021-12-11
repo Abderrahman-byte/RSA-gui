d4948871fadaab8fd3aaf49328041725fc19af99
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-11 20:30:11 +0100

    Generate keys

--- include/widgets/RsaForm.hpp
+++ include/widgets/RsaForm.hpp
@@ -1,11 +1,27 @@
 #include <qt5/QtWidgets/QWidget>
+#include <qt5/QtWidgets/QLineEdit>
+
+#include "widgets/InputField.hpp"
+#include "widgets/TextField.hpp"
+#include "widgets/Button.hpp"
 
 #ifndef _RSAFORM_H_
 #define _RSAFORM_H_ 1
 
 class RsaForm : public QWidget {
+    Q_OBJECT
+
+    private :
+        InputField *qInputField;
+        InputField *pInputField;
+        InputField *eInputField;
+        InputField *dInputField;
+
     public :
         RsaForm (QWidget *parent = 0);
+
+    public slots :
+        void generateKeys ();
 };
 
 #endif
