579d325583dfbc839bfcb7682f3073d853a19824
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-11 20:32:07 +0100

    Add protection from header re-difinitions

--- include/widgets/TextField.hpp
+++ include/widgets/TextField.hpp
@@ -2,8 +2,13 @@
 
 #include <qt5/QtWidgets/QWidget>
 
+#ifndef _TEXTFIELD_H_
+#define _TEXTFIELD_H_ 1
+
 class TextField : public QWidget {
     public :
         TextField (QWidget * = 0);
         TextField (std::string, std::string = "", QWidget * = 0);
 };
+
+#endif
