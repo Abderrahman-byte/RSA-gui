a7c29fe0fc797904139c4c5ce6e0aadc2751f62f
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-12 12:15:29 +0100

    Link rsa form signals with warning display

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,15 +2,26 @@
 #include <qt5/QtWidgets/QVBoxLayout>
 #include <qt5/QtWidgets/QGridLayout>
 
-#include "widgets/RsaForm.hpp"
 #include "MainWindow.hpp"
+#include "widgets/RsaForm.hpp"
+#include "widgets/WarnDisplay.hpp"
 
 MainWindow::MainWindow (QWidget *parent) {
-    QGridLayout *layout = new QGridLayout;
+    // QGridLayout *layout = new QGridLayout;
+    QVBoxLayout *layout = new QVBoxLayout;
     RsaForm *rsaForm = new RsaForm;
+    WarnDisplay *warnDisplay = new WarnDisplay;
     
     layout->setAlignment(Qt::AlignTop);
-    layout->addWidget(rsaForm, 0, 0, 0, 0);
+    // layout->addWidget(rsaForm, 0, 0, 0, 0);
+    layout->addWidget(rsaForm);
+    layout->addWidget(warnDisplay);
+
+    layout->insertStretch(-1, 1);
+
+    // Connect Form with warning display
+    connect (rsaForm, SIGNAL(warning(QString)), warnDisplay, SLOT(displayWarning(QString)));
+    connect (rsaForm, SIGNAL(fieldChanged()), warnDisplay, SLOT(clearWarning()));
 
     this->setLayout(layout);
     this->setWindowTitle(QCoreApplication::applicationName());
